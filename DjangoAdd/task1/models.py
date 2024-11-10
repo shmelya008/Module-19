a5f079af9f55a41572aae57a980f37eae1808304
shmelya008 <shmelya008@gmail.com>
2024-11-11 00:12:43 +0300

    11.11

--- DjangoAdd/task1/models.py
+++ DjangoAdd/task1/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Buyer(models.Model):
     name = models.CharField(max_length=30)
-    balance = models.DecimalField(max_digits=2, decimal_places=5)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     age = models.IntegerField()
 
     def __str__(self):
@@ -14,8 +14,11 @@ class Buyer(models.Model):
 
 class Game(models.Model):
     title = models.CharField(max_length=100, default='text')
-    cost = models.DecimalField(max_digits=2,decimal_places=5)
-    size = models.DecimalField(max_digits=2,decimal_places=5)
+    cost = models.DecimalField(max_digits=10,decimal_places=2)
+    size = models.DecimalField(max_digits=10,decimal_places=2)
     description = models.TextField(default='text')
     age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer, related_name='games')
+
+    def __str__(self):
+        return self.title
